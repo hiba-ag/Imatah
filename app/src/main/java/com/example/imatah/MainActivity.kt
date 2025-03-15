b16213578f2d9ce418746ee79eae6a94ef95a10c
hiba-ag <hibaag131104@gmail.com>
2025-03-16 00:25:29 +0300

    TP1

--- app/src/main/java/com/example/imatah/MainActivity.kt
+++ app/src/main/java/com/example/imatah/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
 import com.example.imatah.ui.theme.ImatahTheme
 
 class MainActivity : ComponentActivity() {
